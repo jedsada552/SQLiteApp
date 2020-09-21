5f38f5043e48cf07b4e71801b276be1aac22a354
jedsada552 <64512380+jedsada552@users.noreply.github.com>
2020-09-21 13:02:51 +0700

    AddPjSQLite
    SQLite Homework ADD

---
+++ 22.SQLiteApp/DataAccess.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace _22.SQLiteApp
+{
+    class DataAccess
+    {
+        internal static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=SQliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customer (UID INTEGER PRIMARY KEY, " +
+                    "First_name NVARCHAR(35)NULL," +
+                    "Last_name NVARCHAR(35)NULL," +
+                    "Email NVARCHAR(40)NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string UID, string First_name, string Last_name, string Email)
+        {
+            using (SqliteConnection db =
+            new SqliteConnection("Filename=SQliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO Customer VALUES (@UID, @First_name, @Last_name,@Email);";
+                insertCommand.Parameters.AddWithValue("@UID", UID);
+                insertCommand.Parameters.AddWithValue("@First_name", First_name);
+                insertCommand.Parameters.AddWithValue("@Last_name", Last_name);
+                insertCommand.Parameters.AddWithValue("@Email", Email);
+
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=SQliteSample.db"))
+            {
+                db.Open();
+
+               // SqliteCommand selectCommand = new SqliteCommand
+                    //("SELECT Text_Entry from MyTable", db);
+
+               // SqliteDataReader query = selectCommand.ExecuteReader();
+
+                SqliteCommand selectCommand = new SqliteCommand("select * from Customer", db);
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
+        
+    
+
---
+++ 22.SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22.SQLiteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void Txt_Entry_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(TxT_ID.Text, TxT_Name.Text, TxT_Lastname.Text, TxT_Email.Text);
+            string Data = "";
+            foreach (string sh in DataAccess.GetData())
+            {
+                Data = Data +" "+ sh + "\n";
+            }
+            MessageBox.Show(Data);
+            
+        }
+    }
+}
